79704f234700bfd69e07f7401a511541866be733
Damilare <dharjeezy@gmail.com>
2022-07-05 22:02:08 +0100

    increase endowment funds

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -142,7 +142,7 @@ fn testnet_genesis(
 		},
 		balances: BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1u128 << 50)).collect(),
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1u128 << 100)).collect(),
 		},
 		sudo: SudoConfig {
 			// Assign network admin rights.
